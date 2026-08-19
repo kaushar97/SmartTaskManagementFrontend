fd4e4b056c32a310aa9b369106167497e34d8cab
Md Kaushar <mdkaushar416@gmail.com>
2026-08-19 20:32:23 +0530

    Register page changes pushed.

--- src/features/auth/pages/RegisterPage.tsx
+++ src/features/auth/pages/RegisterPage.tsx
@@ -1,5 +1,27 @@
-const RegisterPage = () => {
-  return <h1>Register Page</h1>;
-};
+import { Link, useNavigate } from "react-router-dom";
 
-export default RegisterPage;
+import RegisterForm from "../components/RegisterForm";
+
+export default function RegisterPage() {
+    const navigate = useNavigate();
+
+    const handleRegisterSuccess = () => {
+        navigate("/login");
+    };
+
+    return (
+        <div>
+            <h1>Create Account</h1>
+
+            <RegisterForm
+                onSuccess={handleRegisterSuccess}
+            />
+            <p>
+    Already have an account?{" "}
+    <Link to="/login">
+        Login
+    </Link>
+</p>
+        </div>
+    );
+}
